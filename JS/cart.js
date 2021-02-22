3ab48637305723e2add97696134b6b8c5aa094b2
romainmontagnon <montagnon.romain@gmail.com>
2021-02-22 15:58:20 +0100

    Refactoring de la navbar, just one little checkbox to hide

--- JS/cart.js
+++ JS/cart.js
@@ -20,23 +20,16 @@ function minusOneToCart(){
     showNumberOfCartItems();
 };
 
+function clearCart (){
+    console.log("appel fonction clear");
+    numberOfItems = 0;
+    showNumberOfCartItems();
+}
+
 function showNumberOfCartItems(){
     document.getElementById('numberOfCartItems').innerText=numberOfItems;
     console.log(numberOfItems);
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 document.getElementById('cart-plus-one').addEventListener('click', e=>{
     console.log("+1");
@@ -45,4 +38,9 @@ document.getElementById('cart-plus-one').addEventListener('click', e=>{
 document.getElementById('cart-minus-one').addEventListener('click', e=>{
     console.log("-1");
     minusOneToCart();
+});
+
+document.getElementById('clear-cart').addEventListener('click', e=>{
+    console.log("CLEAR CART");
+    clearCart();
 });
---
+++ JS/navbar.js
@@ -0,0 +1,29 @@
+function showDropdownMenu (){
+    document.getElementById('myDropdown').classList.toggle('show');
+    console.log('show menu');
+};
+
+function showMenu(event, list){
+    console.log(list);
+    if (event===true){
+        list.classList.replace('hide-menu','show-menu');
+    } else if (event===false) {
+        list.classList.replace('show-menu','hide-menu');
+    };
+};
+
+
+let menuDrop = document.getElementById('menu-drop');
+menuDrop.addEventListener('click', e=>{
+    let list = document.getElementById('myDropdown')
+    showMenu(menuDrop.checked, list);
+    console.log(menuDrop.checked);
+    console.log(e);
+});
+
+let checkboxShowMenu=document.getElementById('menu-btn');
+checkboxShowMenu.addEventListener('click', e=>{
+    let list = document.getElementById('navbar-fold');
+    showMenu(checkboxShowMenu.checked, list);
+    console.log(checkboxShowMenu.checked);
+});
