ef8bc38da3f0cede8c17ab3666d2d029a3de0fc9
romainmontagnon <montagnon.romain@gmail.com>
2021-03-09 16:01:38 +0100

    regEx refactoring, validation OK, problemes avec envoie des data et redirection vers page finale

--- JS/payment.js
+++ JS/payment.js
@@ -1,3 +1,10 @@
+const updateRegexCardDate = () =>{
+    let year = (new Date()).getFullYear().toString();
+    let a = year.substring(2,3);    console.log(a);
+    let b = year.substring(3,4);
+    return '^([0-9][1-2]\/['+a+'-9]['+b+'-9])$';
+};
+
 let contact = {
     gender          : '',
     emailAdress     : '',
@@ -11,10 +18,74 @@ let contact = {
     country         : '',
     cardName        : '',
     cardCrypto      : '',
-    cardNumber      : ''
+    cardNumber      : '',
+    cardDate        : ''
 };
 
 let paiementPossible = false;
+
+let gender          = document.querySelector("input[name='gender']:checked");
+let emailAdress     = document.getElementById('emailAdress');
+let phone           = document.getElementById('phone');
+let firstName       = document.getElementById('firstName');
+let lastName        = document.getElementById('lastName');
+let streetNumber    = document.getElementById('streetNumber');
+let streetName      = document.getElementById('streetName');
+let zipCode         = document.getElementById('zipCode');
+let city            = document.getElementById('city');
+let country         = document.getElementById('country');
+let cardName        = document.getElementById('cardName');
+let cardCrypto      = document.getElementById('cardCrypto');
+let cardNumber      = document.getElementById('cardNumber');
+let cardDate        = document.getElementById('cardDate');
+
+let requiredForm    = document.getElementById('requiredForm');
+
+let array = [
+    emailAdress     = emailAdress,
+    phone           = phone,
+    firstName       = firstName,
+    lastName        = lastName,
+    streetNumber    = streetNumber,
+    streetName      = streetName,
+    zipCode         = zipCode,
+    city            = city,
+    country         = country,
+    cardName        = cardName,
+    cardCrypto      = cardCrypto,
+    cardNumber      = cardNumber,
+    cardDate        = cardDate
+];
+
+let regexEmailAdress     = '^([a-zA-Z0-9.]{0,255})@([a-zA-Z0-9.]{0,255})$';
+let regexPhone           = '^(([0-9]{10})|([0-9]{2} *[0-9]{2} *[0-9]{2} *[0-9]{2} *[0-9]{2}))$';
+let regexFirstName       = '^[A-Za-zÀ-ÿ\u00f1\u00d1 \-]*$';
+let regexLastName        = '^[A-Za-zÀ-ÿ\u00f1\u00d1 \-]*$';
+let regexStreetNumber    = '^[0-9]+$';
+let regexStreetName      = '^[0-9A-Za-zÀ-ÿ\u00f1\u00d1 \-]*$';
+let regexZipCode         = '^([0-9]{5})';
+let regexCity            = '^[A-Za-zÀ-ÿ\u00f1\u00d1 \-]*$';
+let regexCountry         = '^[A-Za-zÀ-ÿ\u00f1\u00d1 \-]*$';
+let regexCardName        = '^[A-Za-z0-9 ]*$';
+let regexCardCrypto      = '^[0-9]{3}';
+let regexCardNumber      = '^(([0-9]{16})|([0-9]{4} *[0-9]{4} *[0-9]{4} *[0-9]{4}))$';
+let regexCardDate        = updateRegexCardDate();
+
+const regexFormPattern = () => {
+    emailAdress.setAttribute('pattern', regexEmailAdress);
+    phone.setAttribute('pattern', regexPhone);
+    firstName.setAttribute('pattern', regexFirstName);
+    lastName.setAttribute('pattern', regexLastName);
+    streetNumber.setAttribute('pattern', regexStreetNumber);
+    streetName.setAttribute('pattern', regexStreetName);
+    zipCode.setAttribute('pattern', regexZipCode);
+    city.setAttribute('pattern', regexCity);
+    country.setAttribute('pattern', regexCountry);
+    cardName.setAttribute('pattern', regexCardName);
+    cardCrypto.setAttribute('pattern', regexCardCrypto);
+    cardNumber.setAttribute('pattern', regexCardNumber);
+    cardDate.setAttribute('pattern', regexCardDate);
+};
 
 const storeToSessionStorage = (items, sessionStorageKey) =>{
     // console.table(items)
@@ -23,43 +94,12 @@ const storeToSessionStorage = (items, sessionStorageKey) =>{
 };
 
 const lireFormulaireClient = () => {
-    let gender          = document.querySelector("input[name='gender']:checked");
-    let emailAdress     = document.getElementById('emailAdress');
-    let phone           = document.getElementById('phone');
-    let firstName       = document.getElementById('firstName');
-    let lastName        = document.getElementById('lastName');
-    let streetNumber    = document.getElementById('streetNumber');
-    let streetName      = document.getElementById('streetName');
-    let zipCode         = document.getElementById('zipCode');
-    let city            = document.getElementById('city');
-    let country         = document.getElementById('country');
-    let cardName        = document.getElementById('cardName');
-    let cardCrypto      = document.getElementById('cardCrypto');
-    let cardNumber      = document.getElementById('cardNumber');
-    let requiredForm    = document.getElementById('requiredForm');
-    
-    let array = [
-        emailAdress     = emailAdress,
-        phone           = phone,
-        firstName       = firstName,
-        lastName        = lastName,
-        streetNumber    = streetNumber,
-        streetName      = streetName,
-        zipCode         = zipCode,
-        city            = city,
-        country         = country,
-        cardName        = cardName,
-        cardCrypto      = cardCrypto,
-        cardNumber      = cardNumber
-    ];
-    
     console.log(array);
-
     /*
     Si les champs ne sont pas validés, ils s'afficheront encadré en rouge
     */
     for (let i = 0; i < array.length; i++) {
-        let checkValidityTest = array[i].checkValidity(array[i])
+        let checkValidityTest = array[i].checkValidity(array[i]);
         if (checkValidityTest){
             console.log(array[i].value);
             console.log(array[i].id);
@@ -92,6 +132,7 @@ const lireFormulaireClient = () => {
         contact.cardName       = cardName.value;
         contact.cardCrypto     = cardCrypto.value;
         contact.cardNumber     = cardNumber.value;
+        contact.cardDate       = cardDate.value;
 
         storeToSessionStorage(contact, 'contact');
     } else {
@@ -100,7 +141,7 @@ const lireFormulaireClient = () => {
     console.log(contact);
 };
 
-const checkValidityTest = (array) =>{
+const checkValidityTest = (array) => {
     /*
     si n'importe lequel des champs n'est pas valide, on excute ce code qui permet d'encadrer en rouge la :
     "* : pour le bon déroulement de votre commande, ses champs sont obligatoires."
@@ -109,6 +150,7 @@ const checkValidityTest = (array) =>{
         let checkValidityTest = array[i].checkValidity(array[i])
         if (checkValidityTest){
             array[i].classList.remove('noValidForm');
+            requiredForm.classList.remove('noValidForm');
             paiementPossible = true;
             return true;
         } else if (!checkValidityTest) {
@@ -129,24 +171,38 @@ const validationFormulaireListener = () => {
     } else {
         validationFormulaire.addEventListener('click', e=>{
             console.log('hello');
-            e.preventDefault();
+            //e.preventDefault();
             lireFormulaireClient();
             if(!paiementPossible){
                 e.preventDefault();
                 validationFormulaire.classList.add('noValidForm');
-                //alert("N'oubliez pas de valider vos infos personnelles");
-            } else {
-                fetch("http://localhost:3000/api/order",{
-                    method: 'PUT',
-                    body: JSON.stringify(contact),
-                    headers: {'Content-type': 'application/json; charset=UTF-8',},
-                })
-                .then((response) => response.json())
-                .then((json) => console.log(json))
-                .catch(error => console.log('error', error));
+                alert("N'oubliez pas de valider vos infos personnelles");
+            } else if(paiementPossible){
+                //redirectionJsToUrl ();
+                let url = 'http://localhost:3000/api/order';
+                console.log(url);
+                let contactToString = JSON.stringify(contact);
+                console.log(contactToString);
+                let arrayCartItemsCallback = localStorage.getItem('arrayCartItems');
+                console.log(arrayCartItemsCallback);
+
+                //fetch("http://localhost:3000/api/order",{
+                //     method: 'PUT',
+                //     body: JSON.stringify(contact),
+                //     headers: {'Content-type': 'application/json; charset=UTF-8',},
+                // })
+                // .then((response) => response.json())
+                // .then((json) => console.log(json))
+                // .catch(error => console.log('error', error));
             };
         });
     };
 };
 
+const redirectionJsToUrl = () => {
+  document.location.href="../html/payment.html"; 
+};
+
 document.onload = validationFormulaireListener();
+document.onload = regexFormPattern();
+document.onload = updateRegexCardDate();
